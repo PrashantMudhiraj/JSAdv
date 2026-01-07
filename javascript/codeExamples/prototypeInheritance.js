5827f10b2c28268d586c37b1abf1df15746fe243
CodeSandbox <support@codesandbox.io>
2026-01-07 06:44:22 +0000

    Sandbox snapshot commit

--- javascript/codeExamples/prototypeInheritance.js
+++ javascript/codeExamples/prototypeInheritance.js
@@ -3,6 +3,14 @@
  * Prototype will have its own Prototype, making what's calld prototype chain.
  */
 
+Array.prototype.logData = function () {
+  this.map((x) => console.log(x));
+};
+const arr = [1, 2, 3, 4];
+
+arr.logData();
+arr.entries().forEach(([index, value]) => console.log(index, value)); // works in latest brower, not old or < less than node 20)
+
 const obj1 = {
   city: "Hyderabad",
   name: "Prashant",
@@ -12,5 +20,13 @@ const obj1 = {
 };
 
 obj1.greet();
-console.log(obj1.__proto__);
-console.log(obj1.toString());
+// console.log(obj1.__proto__);
+// console.log(obj1.toString());
+
+/**
+ * obj1.__proto__ -> Object prototype
+ * obj1.__proto__.__proto__ -> null
+ * hello.__proto__ -> function
+ * hello.__proto__.__proto__ -> Object
+ * hello.__proto__.__proto__.__proto__ ->null
+ */

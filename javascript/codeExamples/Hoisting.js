ddbb317e61535ed54e7e5fccddc80fe2b1658131
CodeSandbox <support@codesandbox.io>
2026-01-08 03:47:24 +0000

    Sandbox snapshot commit

--- javascript/codeExamples/Hoisting.js
+++ javascript/codeExamples/Hoisting.js
@@ -1,10 +1,10 @@
 /**
  *  HOISTING : Values of the variables can be accessible before its declartion. It is due to behaviour of JavaScript, When a program is executed, it will run in 2 phases, the 1st phase is memory creation phase where all variable and function declations will be stored and in the 2nd phase code execution will happen. So, here all variable were available in memory to access(heap). whenever we try to access a varaible before its declartion, It will try to check in memory and if available, then return undefined(placeholder).
  * var is hoisted, Can be accessible from anywhere in the program (with undefined placeholder)
- * let is hoisted, But it is in temporary dead zone, can not be accessible -Reference Error
- * const is hoisted, But it is in temporary dead zone, can not be accessible
+ * let is hoisted, But it is in temporal dead zone, can not be accessible -Reference Error
+ * const is hoisted, But it is in temporal dead zone, can not be accessible
  *
- *
+ *Temporal Dead Zone : The time between variable declartion and value assigment to that variable
  */
 
 // getName(); // "Hello World"

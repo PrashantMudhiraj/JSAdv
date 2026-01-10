7ea17b524ea81d2e09b7057e18a1105300beeb6f
PrashantMudhiraj <prashantchevula@gmail.com>
2026-01-10 19:01:23 +0530

    10/01 commit

--- _buffers.js
+++ _buffers.js
@@ -3,20 +3,20 @@ const buffer = new ArrayBuffer(8);
 // console.log(buffer);
 
 const float = {
-  int_8: new Int8Array(buffer), //  8/8 = 1 byte per element = 8 bytes -> 8/1 = 8
-  uint_8: new Uint8Array(buffer), // 8/8 = 1 byte per element = 8 bytes -> 8/1 = 8
-  int_16: new Int16Array(buffer), // 16/8 = 2 byte per element = 4 bytes -> 16/2 = 8
-  uint_16: new Uint16Array(buffer), // 16/8 = 2 byte per element = 4 bytes -> 16/2 = 8
-  uint_32: new Uint32Array(buffer), // 32/8 = 4 byte per element = 2 bytes -> 32/4 = 8
-  float_32: new Float32Array(buffer), // 32/8 = 4 byte per element = 2 bytes -> 32/4 = 8
-  bint_64: new BigUint64Array(buffer), // 64/8 = 8 byte per element = 1 bytes -> 64/8 = 8
+    int_8: new Int8Array(buffer), //  8/8 = 1 byte per element = 8 bytes -> 8/1 = 8
+    uint_8: new Uint8Array(buffer), // 8/8 = 1 byte per element = 8 bytes -> 8/1 = 8
+    int_16: new Int16Array(buffer), // 16/8 = 2 byte per element = 4 bytes -> 16/2 = 8
+    uint_16: new Uint16Array(buffer), // 16/8 = 2 byte per element = 4 bytes -> 16/2 = 8
+    uint_32: new Uint32Array(buffer), // 32/8 = 4 byte per element = 2 bytes -> 32/4 = 8
+    float_32: new Float32Array(buffer), // 32/8 = 4 byte per element = 2 bytes -> 32/4 = 8
+    bigint_64: new BigUint64Array(buffer), // 64/8 = 8 byte per element = 1 bytes -> 64/8 = 8
 };
 float.int_16[0] = 258;
 float.int_16[1] = 123;
 console.log(float);
 const no_of_bytes = {
-  int_8: Int8Array.BYTES_PER_ELEMENT,
-  uint_8: Uint8Array.BYTES_PER_ELEMENT,
-  uint_16: Uint16Array.BYTES_PER_ELEMENT,
+    int_8: Int8Array.BYTES_PER_ELEMENT,
+    uint_8: Uint8Array.BYTES_PER_ELEMENT,
+    uint_16: Uint16Array.BYTES_PER_ELEMENT,
 };
 console.log(no_of_bytes);
